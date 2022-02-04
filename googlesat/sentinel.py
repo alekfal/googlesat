6b6aa70a4920620e89d8a05c0cd4598ef9f47b54
alekfal <alek.falagas@gmail.com>
2022-02-04 19:59:20 +0200

    Updates

--- googlesat/sentinel.py
+++ googlesat/sentinel.py
@@ -1,6 +1,6 @@
 import os
 import datetime as dt
-
+import pandas as pd
 from .utils import extract, get_cache_dir, downloader, create_connection, fill_database
 
 # Sentinel 2 metadata index file links to GCP
@@ -11,7 +11,7 @@ METADATA_URL = {'L1C': 'http://storage.googleapis.com/gcp-public-data-sentinel-2
 # Setting available options
 OPTIONS = ['L2A', 'L1C']
 
-def get_metadata(filename:str = 'index.csv.gz', level:str = 'L2A', force_update = False):
+def get_metadata(filename:str = 'index.csv.gz', level:str = 'L2A', force_update:bool = False):
     
     if level not in OPTIONS:
         raise ValueError("L2A (BOA) or L1C (TOA) are the only available levels.")
@@ -30,15 +30,15 @@ def get_metadata(filename:str = 'index.csv.gz', level:str = 'L2A', force_update 
                 db_file = os.path.join(cache, f"db_{level}.db")
                 conn = create_connection(db_file)
                 file, metadata = extract(filename)
-                fill_database(conn, metadata)
+                fill_database(conn, metadata, name = f"Sentinel-2_{level}")
+                conn.close()
             else:
                 db_file = os.path.join(cache, f"db_{level}.db")
-                if os.path.exists(db_file):
-                    conn = create_connection(db_file)
-                else:
+                if not os.path.exists(db_file):
                     conn = create_connection(db_file)
                     file, metadata = extract(filename)
-                    fill_database(conn, metadata)
+                    fill_database(conn, metadata, name = f"Sentinel-2_{level}")
+                    conn.close()
         else:
             raise FileNotFoundError(f"Could not found {filename}.")
 
@@ -47,7 +47,21 @@ def get_metadata(filename:str = 'index.csv.gz', level:str = 'L2A', force_update 
         db_file = os.path.join(cache, f"db_{level}.db")
         conn = create_connection(db_file)
         file, metadata = extract(filename)
-        fill_database(conn, metadata)
+        fill_database(conn, metadata, name = f"Sentinel-2_{level}")
+        conn.close()
     else:
         raise ValueError("Argument force_update is bool.")
+    
+    return db_file
 
+def query(db_file:str, table:str, cc_limit, date_start, date_end, tile):
+    conn = create_connection(db_file)
+    cur = conn.cursor()
+    try:
+        print(table)
+        print(date_start)
+        current_query = f'SELECT BASE_URL, CLOUD_COVER, SENSING_TIME, MGRS_TILE from "{table}" WHERE MGRS_TILE = "{tile}" AND CLOUD_COVER <= {cc_limit} and date("SENSING_TIME") BETWEEN date("{date_start}") AND date("{date_end}")'
+        result = pd.read_sql(current_query, conn)    
+        print(result)
+    finally:
+        cur.close()
