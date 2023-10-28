8f7ecd8b41dd83ede27b1fe8f1cc5e74bb2dea38
alekfal <alek.falagas@gmail.com>
2023-10-28 15:36:02 +0300

    Upload more complete test for geometry

--- tests/test_geometry_from_file.py
+++ tests/test_geometry_from_file.py
@@ -1,11 +1,16 @@
 import pytest
 from googlesat.sentinel import geometry_from_file
+import geopandas as gpd
 
-geometry_from_file_params = [("./data/test.geojson")]
+gdf = gpd.read_file("./data/test.geojson") 
+fake_list = ["fake_file.geojson"]
+geometry_from_file_params = [("./data/test.geojson"), (gdf), (fake_list)]
 
 @pytest.mark.parametrize("geometry", geometry_from_file_params,)
 def test_geometry_from_file(geometry):
-    tiles = geometry_from_file(geometry)
-    
-    assert isinstance(tiles, list)
-    assert tiles == ['34SFJ','34SGJ','34TFK', '34TGK', '35SKD', '35TKE']
+    try:
+        tiles = geometry_from_file(geometry)
+        assert isinstance(tiles, list)
+        assert tiles == ['34SFJ','34SGJ','34TFK', '34TGK', '35SKD', '35TKE']
+    except TypeError:
+        pass
